552750881c3a185da1c2a97d0bd373d338849a30
Stoia <alin58236@gmail.com>
2022-11-13 03:44:25 +0200

    WIP Created database

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,13 +1,10 @@
 package com.example.demo.student;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 //this class will serve as the API for students (routes info)
